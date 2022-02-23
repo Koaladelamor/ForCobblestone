4b82a001d0c44324e6cabe72f324862170ec0aba
Tins33 <noceda14@gmail.com>
2022-02-23 20:21:37 +0100

    Interaccion con enemigos en FreeRoam
    aún queda retocar que el enemigo se dirija hacia tí cuando te ve

---
+++ Assets/Martí/Scripts/PlayerVision.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVision : MonoBehaviour
+{
+    public bool enemyDetected;
+
+    private GameObject[] enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        enemyDetected = false;
+
+        enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int i = 0; i < enemy.Length; i++)
+        {
+            enemy[i].GetComponent<SpriteRenderer>().enabled = false;
+        }
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (enemyDetected)
+        {
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                enemy[i].GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+        */
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            
+            //enemyDetected = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            //enemyDetected = false;
+            other.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
