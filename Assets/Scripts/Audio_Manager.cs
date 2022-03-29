544b4046fbd85477f90bb80a967351ec18b525f1
Tins33 <noceda14@gmail.com>
2022-03-29 18:00:34 +0200

    Audio Manager 1.0

--- Assets/Scripts/Audio_Manager.cs
+++ Assets/Scripts/Audio_Manager.cs
@@ -1,79 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Audio_Manager : MonoBehaviour
 {
 
-    static Audio_Manager mAudio;
+    [SerializeField] private AudioSource effectsSounds;
+    [SerializeField] private AudioSource BackgroungMusic;
 
-    static public Audio_Manager Instance
-    {
-        get { return mAudio; }
-        private set { }
-    }
+    [SerializeField] private AudioClip selectSound;
+    [SerializeField] private AudioClip pressedSound;
 
-    public GameObject anyButton;
+    [SerializeField] private AudioClip enemyHurt;
+    [SerializeField] private AudioClip playerHurt;
 
-    public AudioSource buttonSounds;
-    public AudioSource BackgroungMusic;
-
-    public AudioClip menuMusic;
-
-    public AudioClip selectSound;
-    public AudioClip pressedSound;
-
-    public AudioClip enemyHurt;
-    public AudioClip playerHurt;
-
-    public AudioClip lifeSound;
+    [SerializeField] private AudioClip lifeSound;
 
     //AudioClip[] 
 
     //Hacer enum de audio sources
 
-    public enum BackgroundSongs { NONE, MENUAUDIO, EXPLOREAUDIO, BATTLEAUDIO };
+    public enum AudioEffects { NONE, SELECT, PRESSED, ENEMYHURT, PLAYERHURT, LIFESOUND };
 
-    public BackgroundSongs canciones;
+    //*****************************************
+    public void PlayEffect(AudioEffects effect) {
 
-    private void Start()
-    {
-        if (mAudio == null)
+        effectsSounds.Stop();
+
+        switch (effect)
         {
-            mAudio = this;
-            DontDestroyOnLoad(this);
-        }
-        else
-        {
-            Destroy(this.gameObject);
-            return;
-        }
-
-        BackgroungMusic.clip = menuMusic;
-        BackgroungMusic.Play();
-        //BackgroungMusic.
-    }
-
-    private void Update()
-    {
-        switch (canciones)
-        {
-            case BackgroundSongs.NONE:
-
+            case AudioEffects.NONE:
                 break;
-            case BackgroundSongs.MENUAUDIO:
-
-                BackgroungMusic.clip = menuMusic;
-                BackgroungMusic.Play();
-                //BackgroungMusic.
+            case AudioEffects.SELECT:
+                effectsSounds.clip = selectSound;
                 break;
-            case BackgroundSongs.EXPLOREAUDIO:
+            case AudioEffects.PRESSED:
+                effectsSounds.clip = pressedSound;
                 break;
-            case BackgroundSongs.BATTLEAUDIO:
+            case AudioEffects.ENEMYHURT:
+                effectsSounds.clip = enemyHurt;
+                break;
+            case AudioEffects.PLAYERHURT:
+                effectsSounds.clip = playerHurt;
+                break;
+            case AudioEffects.LIFESOUND:
+                effectsSounds.clip = lifeSound;
                 break;
             default:
                 break;
         }
+
+        effectsSounds.Play();
+
+    }
+
+    public void PlaySelect(BaseEventData data)
+    {
+        PlayEffect(AudioEffects.SELECT);
+    }
+
+    public void PlayPressed(BaseEventData data) 
+    {
+        PlayEffect(AudioEffects.PRESSED);
+    }
+
+
+    //public enum BackgroundSongs { NONE, MENUAUDIO, EXPLOREAUDIO, BATTLEAUDIO };
+
+    ////public BackgroundSongs canciones;
+
+    /*
+    private void Update()
+    {
 
     }
 
@@ -105,11 +104,15 @@ public class Audio_Manager : MonoBehaviour
         buttonSounds.clip = lifeSound;
         buttonSounds.Play();
     }
+    */
 
+    /*
     public void PlayTrack(AudioSource _audio) {
         _audio.Play();
     }
+    */
 
+    /*
     public void SetBackgroundSong(BackgroundSongs _song) {
 
         canciones = _song;
@@ -117,5 +120,29 @@ public class Audio_Manager : MonoBehaviour
 
 
     }
+    */
 
+    /*
+    private void chooseBackgroundSound() {
+        
+        switch (canciones)
+        {
+            case BackgroundSongs.NONE:
+
+                break;
+            case BackgroundSongs.MENUAUDIO:
+
+                BackgroungMusic.clip = menuMusic;
+                BackgroungMusic.Play();
+                BackgroungMusic.
+                break;
+            case BackgroundSongs.EXPLOREAUDIO:
+                break;
+            case BackgroundSongs.BATTLEAUDIO:
+                break;
+            default:
+                break;
+        }
+    }
+    */
 }
--- Assets/Scripts/ItemToPick.cs
+++ Assets/Scripts/ItemToPick.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public class ItemToPick : MonoBehaviour
 {
     public ItemObject item;
+    private Audio_Manager Audio_Manager;
+
+    private void Start()
+    {
+        Audio_Manager = GameObject.FindObjectOfType<Audio_Manager>();
+    }
+    void Pick()
+    {
+        //Cojer la llave
+        Audio_Manager.PlayEffect(Audio_Manager.AudioEffects.PLAYERHURT);
+    }
 }
